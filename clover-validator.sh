0b6a3c8cd770daab21c0f14d142bc30c334e10a7
Drew Hammond <drew@alphagenetica.com>
2014-08-26 19:01:18 -0400

    Committing basic script boilerplate

---
+++ clover-validator.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Clover Validator
+#
+# Author: Drew Hammond <drew@alphagenetica.com>
+# Fork: https://github.com/drewhammond/clover-validator
+
+function usage() {
+	echo
+	echo 'USAGE: @todo'
+	echo
+}
+
+usage
